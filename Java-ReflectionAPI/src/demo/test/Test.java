5e092baa0cdde441098ef6ce49341a98eae80e17
Md Farhan <farhansiddiquii438@gmail.com>
2024-11-25 16:13:26 +0530

    added

---
+++ Java-ReflectionAPI/src/demo/test/Test.java
@@ -0,0 +1,32 @@
+package demo.test;
+
+import java.lang.reflect.Field;
+
+import demo.user.User;
+
+public class Test {
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws Exception {
+
+		// =========load the class into JVM================
+		Class<?> forName = Class.forName("demo.user.User");
+
+		// ========load the class variable============
+		Field field = forName.getDeclaredField("age");
+
+		// ==========give access for variable=================
+		field.setAccessible(true);
+
+		// ==========getting object from the loaded class==================
+		Object newInstance = forName.newInstance();
+
+		// ===========type casting===========================
+		User user = (User) newInstance;
+		user.getAge(); // before age=10
+
+		// ===============setting value to private variable================
+		field.set(newInstance, 30);
+		user.getAge();// after age=30
+
+	}
+}
---
+++ Java-ReflectionAPI/src/demo/user/User.java
@@ -0,0 +1,9 @@
+package demo.user;
+
+public class User {
+	private Integer age=10;
+	
+	public void getAge() {
+		System.out.println("User's age :"+age);
+	}
+}
